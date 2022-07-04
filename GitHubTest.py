06527da19b843035283bc40c53835c4e9479775e
Mohammadreza Moeini <>
2022-07-04 10:27:23 -0400

    Initial commit

---
+++ GitHubTest.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  4 10:14:41 2022
+
+@author: momoe
+"""
+
+def test(x):
+    print("this is a test with tha valu of x={}".format(x))
+    
+    return x+1
+
